f4291cb85608db9bd8414ce6e9793e407dcbedfb
IoTLabHNU <50616948+IoTLabHNU@users.noreply.github.com>
2019-12-13 12:10:38 +0100

    db update

--- mqttPublishF.py
+++ mqttPublishF.py
@@ -102,7 +102,7 @@ game_id = 0
 global product_List
 
 #=================Gets the product list to update the labels on the interface==========================================================#
-connection = mysql.connector.connect(host='db',  database='admindb', user='root',password='root',auth_plugin='mysql_native_password')
+connection = mysql.connector.connect(host='localhost',  database='admindb', user='pi',password='pi',auth_plugin='mysql_native_password')
 QUERY="SELECT * FROM admindb.products"   #returns all the products
 cur=connection.cursor()
 cur.execute(QUERY)
